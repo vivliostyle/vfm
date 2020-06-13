101ce1ecee0755523382e2ccdd4e349a2f08fdd3
Yasuaki Uechi <y@uechi.io>
2020-06-14 01:56:19 +0900

    chore: add debug mode

--- packages/vfm/src/index.ts
+++ packages/vfm/src/index.ts
@@ -5,6 +5,8 @@ import stringify from 'rehype-stringify';
 import markdown from './revive-parse';
 import revive2rehype from './revive-rehype';
 
+const debugMode = process.env.DEBUG;
+
 export interface StringifyMarkdownOptions {
   stylesheet?: string;
   partial?: boolean;
@@ -21,6 +23,11 @@ export function stringifyMarkdown(
   }
 
   processor.use(stringify);
+
+  if (debugMode) {
+    const inspect = require('unist-util-inspect');
+    console.log(inspect(processor.parse(markdownString)));
+  }
 
   return String(processor.processSync(markdownString));
 }
