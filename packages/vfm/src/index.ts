c78c800d2f8e4721271d5ddb900ef5b2b6396034
Yasuaki Uechi <y@uechi.io>
2020-06-25 00:03:26 +0900

    fix: export parser

--- packages/vfm/src/index.ts
+++ packages/vfm/src/index.ts
@@ -1,5 +1,4 @@
-import unified from 'unified';
-import {VFile} from 'vfile';
+import unified, {Processor} from 'unified';
 import doc from 'rehype-document';
 import rehypeStringify from 'rehype-stringify';
 
@@ -12,27 +11,29 @@ export interface StringifyMarkdownOptions {
   partial?: boolean;
 }
 
-export function process(
-  markdownString: string,
-  {stylesheet = undefined, partial = false}: StringifyMarkdownOptions = {},
-): VFile {
+export function VFM({
+  stylesheet = undefined,
+  partial = false,
+}: StringifyMarkdownOptions = {}): Processor {
   const processor = unified().use(markdown).use(html);
   if (!partial) {
     processor.use(doc, {language: 'ja', css: stylesheet});
   }
   processor.use(rehypeStringify);
 
-  if (debug.enabled) {
-    const inspect = require('unist-util-inspect');
-    debug(inspect(processor.parse(markdownString)));
-  }
-
-  return processor.processSync(markdownString);
+  return processor;
 }
 
 export function stringify(
   markdownString: string,
   options: StringifyMarkdownOptions = {},
 ): string {
-  return String(process(markdownString, options));
+  const processor = VFM(options);
+
+  if (debug.enabled) {
+    const inspect = require('unist-util-inspect');
+    debug(inspect(processor.parse(markdownString)));
+  }
+
+  return String(processor.processSync(markdownString));
 }
