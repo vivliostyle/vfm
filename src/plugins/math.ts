45f2687bd233e2ee977976fcb4c2513d79975267
Yasuaki Uechi <y@uechi.io>
2020-07-02 18:36:31 +0900

    feat: properly handle math

---
+++ src/plugins/math.ts
@@ -0,0 +1,13 @@
+import is from 'hast-util-is-element';
+import katex from 'rehype-katex';
+import { Node } from 'unist';
+import remove from 'unist-util-remove';
+
+const removeMathML = () => (tree: Node) => {
+  remove(tree, (node: any) => {
+    const isKatexMathML = node.properties?.className?.includes('katex-mathml');
+    return is(node, 'span') && isKatexMathML;
+  });
+};
+
+export const plugin = { plugins: [katex, removeMathML] };
--- src/revive-rehype.ts
+++ src/revive-rehype.ts
@@ -1,4 +1,3 @@
-import mathjax from 'rehype-mathjax';
 import raw from 'rehype-raw';
 import slug from 'rehype-slug';
 import remark2rehype from 'remark-rehype';
@@ -6,12 +5,13 @@ import unified from 'unified';
 import {handler as figure} from './plugins/figure';
 import {handler as ruby} from './plugins/ruby';
 import {inspect} from './utils/debug';
+import { plugin as math } from './plugins/math';
 
 export default [
   [remark2rehype, {allowDangerousHtml: true, handlers: {ruby}}],
   raw,
-  mathjax,
   figure,
+  math,
   slug,
   inspect('hast'),
 ] as unified.PluggableList<unified.Settings>;
