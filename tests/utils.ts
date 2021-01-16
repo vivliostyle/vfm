f727f547e0d2ab81fcac9a339c0c018019db1974
akabeko <akabekobeko@gmail.com>
2021-01-16 19:31:46 +0900

    chore: Add VFM options to buildProcessorTestingCode #59

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,12 +1,19 @@
 import unistInspect from 'unist-util-inspect';
-import { VFM } from '../src';
+import { StringifyMarkdownOptions, VFM } from '../src';
 
 export const buildProcessorTestingCode = (
   input: string,
   expectedMdast: string,
   expectedHtml: string,
+  options: StringifyMarkdownOptions = {
+    style: undefined,
+    partial: true,
+    title: undefined,
+    language: undefined,
+    replace: undefined,
+  },
 ) => (): any => {
-  const vfm = VFM({ partial: true }).freeze();
+  const vfm = VFM(options).freeze();
   expect(unistInspect.noColor(vfm.parse(input))).toBe(expectedMdast.trim());
   expect(String(vfm.processSync(input))).toBe(expectedHtml);
 };
