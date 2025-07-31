6719ccc23ca4bf8318c75e7a7cb0270824cbb1de
Koutaro Mukai <mukai.k1011k@outlook.jp>
2025-07-31 12:09:35 +0900

    Remove type stubs for modules that now provide their own typings

--- types/remark.d.ts
+++ types/remark.d.ts
@@ -1,28 +1,28 @@
-declare module 'rehype-raw' {
-  import { Plugin } from 'unified';
-  const plugin: Plugin;
-  export default plugin;
-}
+// declare module 'rehype-raw' {
+//   import { Plugin } from 'unified';
+//   const plugin: Plugin;
+//   export default plugin;
+// }
 
 declare module 'hast-util-find-and-replace';
 declare module 'remark-shortcodes';
 // declare module 'hast-util-is-element';
 // declare module 'hast-util-select';
 // declare module 'hastscript';
-declare module 'mdast-util-to-hast/lib/all.js';
-declare module 'mdast-util-to-string';
+// declare module 'mdast-util-to-hast/lib/all.js';
+// declare module 'mdast-util-to-string';
 // declare module 'mdast-util-find-and-replace';
-declare module 'rehype-katex';
-declare module 'remark-slug';
-declare module 'rehype-slug';
+// declare module 'rehype-katex';
+// declare module 'remark-slug';
+// declare module 'rehype-slug';
 declare module 'rehype-format';
 declare module 'remark-breaks';
-declare module 'remark-footnotes';
-declare module 'remark-math';
+// declare module 'remark-footnotes';
+// declare module 'remark-math';
 // declare module 'to-vfile';
-declare module 'unist-util-remove';
+// declare module 'unist-util-remove';
 declare module 'remark-attr';
-declare module 'unist-util-find-after';
+// declare module 'unist-util-find-after';
 declare module 'doctype';
 
 interface Tokenizer {
